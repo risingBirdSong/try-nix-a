4b1d9db5d89919d662912cb26b3d64a0e8b7c075
risingBirdSong <heregoingthere@outlook.com>
2022-05-19 16:02:29 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "aaa";
+
+  inputs = {
+    # Nix Inputs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    pre-commit-hooks,
+    flake-utils,
+  }: let
+    utils = flake-utils.lib;
+  in
+    utils.eachDefaultSystem (system: let
+      supportedGHCVersion = "921";
+      compilerVersion = "ghc${supportedGHCVersion}";
+      pkgs = nixpkgs.legacyPackages.${system};
+      hsPkgs = pkgs.haskell.packages.${compilerVersion}.override {
+        overrides = hfinal: hprev: {
+          aaa = hfinal.callCabal2nix "aaa" ./. {};
+        };
+      };
+    in rec {
+      packages =
+        utils.flattenTree
+        {aaa = hsPkgs.aaa;};
+
+      # nix flake check
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            fourmolu.enable = true;
+            cabal-fmt.enable = true;
+          };
+        };
+      };
+
+      # nix develop
+      devShell = hsPkgs.shellFor {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        withHoogle = true;
+        packages = p: [
+          p.aaa
+        ];
+        buildInputs = with pkgs;
+          [
+            hsPkgs.haskell-language-server
+            haskellPackages.cabal-install
+            cabal2nix
+            haskellPackages.ghcid
+            haskellPackages.fourmolu
+            haskellPackages.cabal-fmt
+            nodePackages.serve
+          ]
+          ++ (builtins.attrValues (import ./scripts.nix {s = pkgs.writeShellScriptBin;}));
+      };
+
+      # nix build
+      defaultPackage = packages.aaa;
+    });
+}
---
+++ shell.nix
@@ -0,0 +1,45 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, async, base, binary, bytestring
+      , containers, deriving-aeson, foldl, hspec, hspec-expectations, lib
+      , primitive, QuickCheck, safe-exceptions, text
+      , unordered-containers, vector
+      }:
+      mkDerivation {
+        pname = "aaa";
+        version = "0.0.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          aeson async base binary bytestring containers deriving-aeson foldl
+          primitive safe-exceptions text unordered-containers vector
+        ];
+        executableHaskellDepends = [
+          aeson async base binary bytestring containers deriving-aeson foldl
+          primitive safe-exceptions text unordered-containers vector
+        ];
+        testHaskellDepends = [
+          aeson async base binary bytestring containers deriving-aeson foldl
+          hspec hspec-expectations primitive QuickCheck safe-exceptions text
+          unordered-containers vector
+        ];
+        license = "unknown";
+        hydraPlatforms = lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
